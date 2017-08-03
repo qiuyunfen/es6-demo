1da98a4e2ff74e39bbddc0c778e01e1d40ae6fba
Yunfen <yfqiu@thoughtworks.com>
2017-08-03 09:40:32 +0800

    TDZ

--- src/test.js
+++ src/test.js
@@ -20,3 +20,15 @@ var foo = 'var';
 //console.log(bar);//bar is not defined
 let bar = 'let';
 
+//如果区块中存在let和const命令，这个区块对这些命令声明的变量，从一开始就形成了封闭作用域。凡是在声明之前就使用这些变量，就会报错
+var tmp = 'abc';
+if(true) {
+    //TDZ开始
+   // tmp = 1;//ReferenceError
+   // console.log(tmp);//ReferenceError
+
+    let tmp;////TDZ结束
+    console.log(tmp);
+    tmp = 'qwe';
+    console.log(tmp);
+}
