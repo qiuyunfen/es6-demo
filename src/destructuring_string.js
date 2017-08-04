01a9cb08cb70687f85a69ba8e09a2e3a782b91bb
Yunfen <yfqiu@thoughtworks.com>
2017-08-04 10:49:05 +0800

    undefined and null can not change to object

--- src/destructuring_string.js
+++ src/destructuring_string.js
@@ -7,3 +7,11 @@ console.log(c);
 //字符串包含length属性
  let {length: len} = 'hello';
  console.log(len);
+
+//当进行解构赋值时，右边表达式如果不是对象，会默认进行转对象操作
+//由于undefined和null无法转为对象，所以对它们进行解构赋值，都会报错
+let {toString: s} = 123;
+console.log(s === Number.prototype.toString); 
+
+let {toString: s1} = false;
+console.log(s1 === Boolean.prototype.toString);
