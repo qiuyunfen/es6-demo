7f86758e1898a0d05495a6884bcd2a7f490bbbcf
Yunfen <yfqiu@thoughtworks.com>
2017-08-04 13:41:30 +0800

    number isNaN

--- src/number.js
+++ src/number.js
@@ -8,8 +8,18 @@ console.log(Number.isFinite('foo')); // false
 console.log(Number.isFinite('15')); // false
 console.log(Number.isFinite(true)); // false
 
+//判读是否为nan
+console.log(Number.isNaN(NaN)) // true
+console.log(Number.isNaN(15)) // false
+console.log(Number.isNaN('15')) // false
+console.log(Number.isNaN(true)) // false
+console.log(Number.isNaN(9/NaN)) // true
+console.log(Number.isNaN('true'/0)) // true
+console.log(Number.isNaN('true'/'true')) // true
+
+//ES5使用如下方式模拟
 (function(global) {
-    console.log(global.isFinite);
+    //console.log(global.isFinite);
     var global_isFinite = global.isFinite;
     Object.defineProperty(Number, 'isFinite', {
         value: function isFinite(value) {
@@ -18,5 +28,19 @@ console.log(Number.isFinite(true)); // false
         configurable: true,
         enumerable: false,
         writable: true
-    })
+    });
+
+    var global_isNaN = global.isNaN;
+    Object.defineProperty(Number, 'isNaN', {
+        value: function isNaN(value) {
+            return typeof value === 'number' && global_isNaN(value);
+        },
+        configurable: true,
+        writable: true,
+        enumerable: false
+    });
 })(this);
+
+
+
+
