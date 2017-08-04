e13d64255023cbcb472c276124e530403d8a7c0c
Yunfen <yfqiu@thoughtworks.com>
2017-08-04 09:43:51 +0800

    not iterator object

--- src/destructuring.js
+++ src/destructuring.js
@@ -18,3 +18,12 @@ console.log(e);
 let [f, g] = [1,2,3];
 console.log(f);
 console.log(g);
+
+//前5个表达式右边转换为对象后是不可迭代的，最后一个表达式，对象本身是不能迭代的，因此会报错
+//let [foo1] = 1;
+//let [foo2] = false;
+//let [foo3] = null;//Cannot read property 'Symbol(Symbol.iterator)' of null
+//let [foo4] = undefined;//Cannot read property 'Symbol(Symbol.iterator)' of undefined
+//let [foo5] = NaN;//undefined is not a function
+//let [foo6] = {};
+
