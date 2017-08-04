e93744600a2450febedba6e30eb9bf89aa04e7b9
Yunfen <yfqiu@thoughtworks.com>
2017-08-04 11:15:12 +0800

    startsWith

--- src/string.js
+++ src/string.js
@@ -8,3 +8,6 @@ for(let codePoint of text) {
 var s = 'Hello world!';
 console.log(s.includes('o')); // true
 console.log(s.includes('Hello', 6));//第二个参数表示从当前位置道字符串结束
+
+console.log(s.startsWith('Hello'));
+console.log(s.startsWith('world', 6));
